62b2e2f1dddc9b888479963437150e5fd3a709e8
Shoji <hshojikato@gmail.com>
2025-05-09 15:39:43 -0300

    atualizar nomes das variáveis de ambiente

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -1,13 +1,13 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER,
-  process.env.PASSWORD,
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    dialect: process.env.DIALECT,
-    host: process.env.HOST,
-    port: process.env.PORT,
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
   }
 );
 
